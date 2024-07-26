86142288a1f89d9468039bf6e3f71e2b6b5103b6
santosh-gh <57346552+santosh-gh@users.noreply.github.com>
2024-07-26 16:23:03 +0100

    update

--- PowerShellLibrary/Add-AdAppRegistrations.ps1
+++ PowerShellLibrary/Add-AdAppRegistrations.ps1
@@ -351,7 +351,7 @@ Function Set-AadApp {
     }
 
     $appObjectId = "ea14266a-4d9e-4674-9f98-08d077ac8d93"
-    New-AzADAppFederatedCredential -ApplicationObjectId $appObjectId -Audience api://AzureADTokenExchange -Issuer https://vstoken.dev.azure.com/0843dc02-bf94-4c0c-b0ed-bb5f8c829f46 -name 'testing01' -Subject 'sc://defragovuk/DEFRA-FFC/test'
+    New-AzADAppFederatedCredential -ApplicationObjectId $appObjectId -Audience api://AzureADTokenExchange -Issuer https://vstoken.dev.azure.com/0843dc02-bf94-4c0c-b0ed-bb5f8c829f46 -name 'testing01' -Subject 'sc://defragovuk/DEFRA-FFC/WQEWQ'
 
 }
 
