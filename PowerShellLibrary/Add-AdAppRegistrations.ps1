d6e75048c3ea79a20b612343961e5a204f8b5186
santosh-gh <57346552+santosh-gh@users.noreply.github.com>
2024-07-29 17:58:37 +0100

    update

--- PowerShellLibrary/Add-AdAppRegistrations.ps1
+++ PowerShellLibrary/Add-AdAppRegistrations.ps1
@@ -520,9 +520,10 @@ Function Add-FederatedCredential() {
 
     #$appid = (az ad app list --display-name "ADO-DefraGovUK-ADP-SND1-ContUAA" | convertFrom-Json).appId
     #Write-Output "app id of  ADO-DefraGovUK-ADP-SND1-ContUAA $appid  "
-    $appObjectId = az keyvault secret show --name ADO-DefraGovUK-ADP-SND1-ContUAA-SP-ObjectId --vault-name $serviceEndpoints.azureRMServiceConnections.keyVault.name --query value
-    Write-Host "appObjectId of ADO-DefraGovUK-ADP-SND2-ContUAA '$appObjectId'"
-    
+    #$appObjectId = az keyvault secret show --name ADO-DefraGovUK-ADP-SND1-ContUAA-SP-ObjectId --vault-name $serviceEndpoints.azureRMServiceConnections.keyVault.name --query value
+    $appObjectId = Get-AzKeyVaultSecret -VaultName $serviceEndpoints.azureRMServiceConnections.keyVault.name -Name ADO-DefraGovUK-ADP-SND1-ContUAA-SP-ObjectId -AsPlainText
+    Write-Host "ADO-DefraGovUK-ADP-SND1-ContUAA-SP-ObjectId '$appObjectId'"
+
     foreach ($app in $apps.applications) {
         #$appObjectId = "ea14266a-4d9e-4674-9f98-08d077ac8d93"
         New-AzADAppFederatedCredential -ApplicationObjectId $appObjectId -Audience $app.federartedCredential.audience -Issuer $app.federartedCredential.issuer -name $app.federartedCredential.name -Subject $app.federartedCredential.subject    
